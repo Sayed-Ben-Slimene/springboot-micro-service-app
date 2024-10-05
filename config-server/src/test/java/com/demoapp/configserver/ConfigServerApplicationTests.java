d09eb77ed6602d78b05d0f6288ee6770f0c278e2
Sayed-Ben-Slimene <sayed.benslimene@esprit.tn>
2024-10-05 20:21:11 +0100

    config discoveryServer/Gateway/Student

---
+++ config-server/src/test/java/com/demoapp/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoapp.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
